233e4bbabac4cff6be3c81483b7604a6ebf15b91
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2024-03-12 22:07:06 +0530

    worked on the search input

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,30 +1,44 @@
+import { useState } from "react"
 import "../styles/chat.scss"
 const Chat = () => {
+  const [search, setSearch] = useState("");
+
+
+  // search =>
+  const inputSearch = (e) => {
+    let { value } = e.target;
+    setSearch(value)
+  }
+
+
   return (
     <div className="chat-container">
       <div className="left-chat-container">
         <h2 className="chit-chat">Chit-Chat</h2>
         <div className="input-search">
           <img src="" alt="" />
-          <input type="text" placeholder="Search user or chat ...." />
+          <input type="text" placeholder="Search user or chat ...." value={search} onChange={inputSearch} />
         </div>
         <div className="chat-lists">
           {
             chatUsers.map((_) => {
-              return (
-                <div className="users">
-                  <div className="users-profile">
-                  </div>
-                  <div className="user-name">
-                    <div className="name">
-                      <b className="user">Jitesh</b>
-                      <span className="chat-time">9m</span>
+              if (_.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                return (
+                  <div className="users">
+                    <div className="users-profile">
                     </div>
-                    <div className="last-chat">ok thanks</div>
+                    <div className="user-name">
+                      <div className="name">
+                        <b className="user">{_}</b>
+                        <span className="chat-time">9m</span>
+                      </div>
+                      <div className="last-chat">ok thanks</div>
+                    </div>
                   </div>
-                </div>
-              )
-            })
+                )
+              }
+            }
+            )
           }
         </div>
       </div>
@@ -39,4 +53,4 @@ const Chat = () => {
 export default Chat;
 
 
-let chatUsers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+let chatUsers = ["Jitesh", "Aman", "Mohit Sir", "Sachin Thapa", "Pushparaj", "Rahul", "Anshul", "Monu", "Saumya", "Prerana", "Anand", "Pragati"]

5d48bc4b1125a25c6e7fc1e9e70ac068c017903b
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2024-03-29 18:42:59 +0530

    worked on toast massage

--- client/src/components/Toastnotification.jsx
+++ client/src/components/Toastnotification.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 export const ToastNotifications = ({ toastMassages }) => {
+    const [showToast, setshowToast] = useState(true);
+    setTimeout(() => {
+        setshowToast(false)
+    }, 2000);
 
-
-    // toast notifications styles =>
-    const toastStyles = {
-        backgroundColor: "#00203FFF",
-        color: "#ADEFD1FF",
-        fontWeight: "600",
-        padding: "7px 14px",
-        fontSize: "18px"
-    }
     return (
-        <div className="toast-massage-container" style={toastStyles}>{toastMassages}</div>
+        <div className="toast-massage-container" style={showToast ? toastStyles : { display: "none" }}>{toastMassages}</div>
     )
 }
+
+// toast notifications styles =>
+const toastStyles = {
+    backgroundColor: "#ADEFD1FF",
+    color: "#00203FFF",
+    fontWeight: "600",
+    padding: "7px 14px",
+    fontSize: "18px",
+    borderRadius: "6px",
+    borderBottom: "solid black",
+    transition: "all 0.7s"
+};
--- client/src/components/auth.jsx
+++ client/src/components/auth.jsx
@@ -4,6 +4,7 @@ import { userAuthorization } from "../APIs/api"
 import { ToastNotifications } from "./Toastnotification"
 
 const Authentication = () => {
+  const [toastMassage, setToastMassage] = useState("")
   const [toastShow, setToastShow] = useState(false);
   const [inputData, setInputData] = useState(
     {
@@ -14,10 +15,6 @@ const Authentication = () => {
   )
   const [auth, setAuth] = useState(true);
 
-
-
-
-  
   // Login or Register =>
   const handleAuth = (e) => {
     e.preventDefault();
@@ -39,45 +36,54 @@ const Authentication = () => {
 
     if (e.target.name === "register") {
       if (!name || !email || !password) {
-        return alert("fill the blank feilds!")
+        setToastShow(true);
+        return setToastMassage("fill the blank feilds!");
       }
       data = { name, email, password }
       userAuthorization(data, "register");
     }
     else if (e.target.name === "login") {
       if (!email || !password) {
-        return alert("fill the blank feilds!")
+        setToastShow(true);
+        return setToastMassage("fill the blank feilds!");
       }
       data = { email, password }
       userAuthorization(data, "login");
     }
   }
-
   return (
     <div className="authentication-container">
+      {
+        toastShow && (
+          <div className="toast-notification-container">
+            <ToastNotifications toastMassages={toastMassage} />
+          </div>
+        )
+      }
 
-      <div className="toast-notification-container">
-        <ToastNotifications toastMassages={"Hello I am the Toast Notifications!"} />
-      </div>
 
       {
         auth ?
-          <div className="container">
-            <h1 className="auth-head">Login</h1>
-            <input type="email" name="email" value={inputData.email} onChange={handleChange} placeholder="Email..." />
-            <input type="password" name="password" value={inputData.password} onChange={handleChange} placeholder="Password..." />
-            <button className="auth-btn" name="login" onClick={handleSubmit}>Submit</button>
-            <a href="/" onClick={handleAuth}>Don't have an account? Register</a>
-          </div>
+          (
+            <div className="container">
+              <h1 className="auth-head">Login</h1>
+              <input type="email" name="email" value={inputData.email} onChange={handleChange} placeholder="Email..." />
+              <input type="password" name="password" value={inputData.password} onChange={handleChange} placeholder="Password..." />
+              <button className="auth-btn" name="login" onClick={handleSubmit}>Submit</button>
+              <a href="/" onClick={handleAuth}>Don't have an account? Register</a>
+            </div>
+          )
           :
-          < div className="container" action="/register" method="post">
-            <h1 className="auth-head">Register</h1>
-            <input type="text" name="name" placeholder="Name" value={inputData.name} onChange={handleChange} />
-            <input type="email" name="email" placeholder="Email" value={inputData.email} onChange={handleChange} />
-            <input type="password" name="password" placeholder="Password" value={inputData.password} onChange={handleChange} />
-            <button className="auth-btn" name="register" onClick={handleSubmit} >Submit</button>
-            <a href="/">have an account? Login</a>
-          </div>
+          (
+            < div className="container" action="/register" method="post">
+              <h1 className="auth-head">Register</h1>
+              <input type="text" name="name" placeholder="Name" value={inputData.name} onChange={handleChange} />
+              <input type="email" name="email" placeholder="Email" value={inputData.email} onChange={handleChange} />
+              <input type="password" name="password" placeholder="Password" value={inputData.password} onChange={handleChange} />
+              <button className="auth-btn" name="register" onClick={handleSubmit} >Submit</button>
+              <a href="/">have an account? Login</a>
+            </div>
+          )
       }
     </div >
   )
